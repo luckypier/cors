e270a90b260ffdd9ad43abfeb78552c6d2e03ff2
Daniel Delgado <dedd1993@gmail.com>
2018-01-24 10:24:03 -0500

    jquery ajax http request added as alternative

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { OauthService } from '../services/oauth.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -39,6 +41,64 @@ export class DashboardComponent implements OnInit {
         console.error('changeCodeForToken response from DashboardComponent', error);
       }
     );
+  }
+
+  testingOauthJq(code) {
+    const d = {
+      'oauth_url': '10.36.70.64:8087',
+      'grant_type': 'authorization_code',
+      'client_id': 'prime-front-end-key',
+      'client_secret': '49b67f2c-c662-11e7-a3b6-0242ac120003',
+      'redirect_uri': 'http://10.36.71.183:4200/dashboard',
+      'response_type': 'code',
+      'code': code
+    };
+
+    // const config = {
+    //   params: {
+    //     grant_type: 'password',
+    //     username: d.client_id,
+    //     password: d.client_secret
+
+    //   },
+    //   headers: {
+    //     Authorization: 'Basic ' + Base64.encode('secret-client' + ':' + 'secret')
+    //   }
+    // };
+
+    // $.post(`http://10.36.70.64:8087/oauth/token?grant_type=authorization_code&client_id=prime-front-end-key&client_secret=49b67f2c-c662-11e7-a3b6-0242ac120003&redirect_uri=http://10.36.71.183:4200/dashboard&code=${code}`,
+    // {},
+    // function(data, status){
+    //     console.log(data);
+    // });
+
+    $.post(`http://10.36.70.64:8087/oauth/token`,
+    {
+      'grant_type': 'authorization_code',
+      'client_id': 'prime-front-end-key',
+      'client_secret': '49b67f2c-c662-11e7-a3b6-0242ac120003',
+      'redirect_uri': 'http://10.36.71.183:4200/dashboard',
+      'code': code,
+    },
+    function(data, status){
+        console.log(data);
+    });
+
+    // const settings = {
+    //   'async': true,
+    //   'crossDomain': true,
+    //   'url': 'http://10.36.70.64:8087/oauth/token',
+    //   'method': 'POST',
+    //   'headers': {
+    //     'content-type': 'application/json'
+    //   },
+    //   'processData': false,
+    //   'data': "{\"grant_type\":\"authorization_code\",\"client_id\": \"prime-front-end-key\",\"client_secret\": \"49b67f2c-c662-11e7-a3b6-0242ac120003\",\"code\": \"ubWVTX\",\"redirect_uri\": \"http://10.36.71.183:4200/dashboard\"}"
+    // };
+
+    // $.ajax(settings).done(function (response) {
+    //   console.log(response);
+    // });
   }
 
 
--- src/app/services/oauth.service.ts
+++ src/app/services/oauth.service.ts
@@ -4,12 +4,27 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OauthService {
+  private headers = new Headers({
+    'Authorization': 'Basic cHJpbWUtZnJvbnQtZW5kLWtleTo0OWI2N2YyYy1jNjYyLTExZTctYTNiNi0wMjQyYWMxMjAwMDM='
+  });
 
   constructor(private http: Http) { }
 
   changeCodeForToken(code) {
-    const url = `http://10.36.70.64:8087/oauth/token?grant_type=authorization_code&client_id=prime-front-end-key&redirect_uri=http://10.36.71.183:4200/dashboard&code=${code}`;
+    const d = {
+      'oauth_url': '10.36.70.64:8087',
+      'grant_type': 'authorization_code',
+      'client_id': 'prime-front-end-key',
+      'client_secret': '49b67f2c-c662-11e7-a3b6-0242ac120003',
+      'redirect_uri': 'http://10.36.71.183:4200/dashboard',
+      'response_type': 'code',
+      'code': code
+    };
 
+    const url = `http://${d.oauth_url}/oauth/token?grant_type=${d.grant_type}&client_id=${d.client_id}&client_secret=${d.client_secret}&redirect_uri=${d.redirect_uri}&code=${d.code}`;
+    // const url = `prime-front-end-key:49b67f2c-c662-11e7-a3b6-0242ac120003@10.36.70.64:8087/oauth/token?grant_type=authorization_code&client_id=prime-front-end-key&redirect_uri=http://10.36.71.183:4200/dashboard&code=fDlIK2
+
+    // return this.http.post(url, {}, {headers: this.headers})
     return this.http.post(url, {})
     .map((response: Response) => {
         const body = response.json();
