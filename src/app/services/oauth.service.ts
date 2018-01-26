7ce6cfee2ee89a96bc228207db7adb0c6e311380
Pierre Recuay <precuay@belatrixsf.com>
2018-01-26 18:27:33 -0500

    jquery post and options

--- src/app/services/oauth.service.ts
+++ src/app/services/oauth.service.ts
@@ -42,8 +42,8 @@ export class OauthService {
     const url = `http://${d.oauth_url}/oauth/token?grant_type=${d.grant_type}&client_id=${d.client_id}&redirect_uri=${d.redirect_uri}&code=${d.code}`;
     // const url = `prime-front-end-key:49b67f2c-c662-11e7-a3b6-0242ac120003@10.36.70.64:8087/oauth/token?grant_type=authorization_code&client_id=prime-front-end-key&redirect_uri=http://10.36.71.183:4200/dashboard&code=fDlIK2
 
-    // return this.http.post(url, {}, {headers: this.headers})
-    return this.http.post(url, {})
+    return this.http.post(url, {}, {headers: this.headers})
+    //return this.http.post(url, {})
     .map((response: Response) => {
         const body = response.json();
         console.log('OauthService response fron client service', body);
@@ -60,11 +60,69 @@ export class OauthService {
       'code': this.data.code
     };
 
-    const url = `http://${d.oauth_url}/oauth/token?grant_type=${d.grant_type}&client_id=${d.client_id}&redirect_uri=${d.redirect_uri}&code=${d.code}`;
+    //const url = `http://${d.oauth_url}/oauth/token?grant_type=${d.grant_type}&client_id=${d.client_id}&redirect_uri=${d.redirect_uri}&code=${d.code}`;
 
+    const url = `http://${d.oauth_url}/oauth/token?grant_type=${d.grant_type}&code=${d.code}&redirect_uri=${d.redirect_uri}&client_id=${d.client_id}`;
+
+/*
     $.post(url, {}, function(data, status){
         console.log(data);
     });
+*/
+
+    /*$.ajax({
+      type: "POST",
+      url: url,
+      data: d ,
+      
+      dataType: 'json',
+      headers: {
+        'AAA': 'BBB',
+        'Referer': 'unsafe-url',
+        "Authorization": "Basic cHJpbWUtZnJvbnQtZW5kLWtleTo0OWI2N2YyYy1jNjYyLTExZTctYTNiNi0wMjQyYWMxMjAwMDM="
+      }  ,
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader ("XXX", "YYY");
+        xhr.setRequestHeader ("Referer", "unsafe-url");
+        xhr.setRequestHeader ("Authorization", "Basic cHJpbWUtZnJvbnQtZW5kLWtleTo0OWI2N2YyYy1jNjYyLTExZTctYTNiNi0wMjQyYWMxMjAwMDM=");
+      },
+      success: function( data, textStatus, jQxhr ){
+                   console.log( data );
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+    });*/
+  /*  
+  $.ajax({
+      url: url,
+      headers: {
+          'Authorization':'Basic cHJpbWUtZnJvbnQtZW5kLWtleTo0OWI2N2YyYy1jNjYyLTExZTctYTNiNi0wMjQyYWMxMjAwMDM=',
+          'Content-Type':'application/json'
+      },
+      method: 'POST',
+      dataType: 'json',
+      success: function(data){
+        console.log('succes: '+data);
+      }
+  });
+*/
+
+ $.ajax({
+      url: url || "/oauth/token?grant_type=password&username=XXXX&password=CCCC",
+      headers: {
+          'Authorization':'Basic cHJpbWUtZnJvbnQtZW5kLWtleTo0OWI2N2YyYy1jNjYyLTExZTctYTNiNi0wMjQyYWMxMjAwMDM=',
+          'Content-Type':'application/json'
+      },
+      method: 'POST',
+      dataType: 'json',
+      success: function(data){
+        console.log('succes: '+data);
+      }
+  });
+
+
+
   }
 
   getData() {
